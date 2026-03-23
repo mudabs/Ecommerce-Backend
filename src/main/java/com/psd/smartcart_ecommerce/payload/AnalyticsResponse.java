a0e97e754ac61be94320da5fe78a3925a7841230
MATHEW-SHERENI <mashereni@gmail.com>
2026-03-23 01:37:06 -0500

    Implementing API for admin dashboard

---
+++ src/main/java/com/psd/smartcart_ecommerce/payload/AnalyticsResponse.java
@@ -0,0 +1,15 @@
+package com.psd.smartcart_ecommerce.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AnalyticsResponse {
+    private String productCount;
+    private String totalRevenue;
+    private String totalOrders;
+
+}
