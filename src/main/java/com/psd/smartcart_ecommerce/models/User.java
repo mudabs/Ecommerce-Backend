f43b65e4402046d67ed8f7070c6f6a96fa76a2d7
mudabs <mudaburamunashe@gmail.com>
2026-04-07 21:39:16 -0500

    add DataInitializer to seed roles on startup and create StripeCheckoutSessionDto for payment processing

--- src/main/java/com/psd/smartcart_ecommerce/models/User.java
+++ src/main/java/com/psd/smartcart_ecommerce/models/User.java
@@ -48,14 +48,20 @@ public class User {
         this.password = password;
     }
 
-    @Setter
-    @Getter
     @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE},
             fetch = FetchType.EAGER)
     @JoinTable(name = "user_role",
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id"))
     private Set<Role> roles = new HashSet<>();
+
+        public Set<Role> getRoles() {
+                return roles;
+        }
+
+        public void setRoles(Set<Role> roles) {
+                this.roles = roles;
+        }
 
     @Getter
     @Setter
